47a1893440746705c4e790b5c3d6f6bc020d0e21
DanielJMorrissey <danieljohnmorrissey@hotmail.com>
2022-07-21 14:54:46 +0100

    finished testing

--- tests/Feature/FeatureTest.php
+++ tests/Feature/FeatureTest.php
@@ -11,11 +11,6 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 class FeatureTest extends TestCase
 {
 
-    /*
-        test database
-        test analysis results
-    */
-
     use DatabaseMigrations;
 
     /**
@@ -279,6 +274,339 @@ class FeatureTest extends TestCase
 
         $this->get('/gpTracker');
         $this->get('/deleteGPVisit/1')->assertSessionHasNoErrors()->assertRedirect('/gpTracker');
+    }
+
+    public function testUserInDatabase()
+    {
+        $this->post('computeRegister', [
+            'username' => 'Ben10',
+            'password' => 'Daniel1',
+            'password1' => 'Daniel1'
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'username' => 'Ben10'
+        ]);
+
+
+    }
+
+    public function testDiaryInDatabase()
+    {
+        $this->post('computeRegister', [
+            'username' => 'Ben10',
+            'password' => 'Daniel1',
+            'password1' => 'Daniel1'
+        ]);
+
+        $this->get('/addDiary');
+        $this->post('addDiary/entry', [
+            'date' => '2022-06-05',
+            'stress' => 1,
+
+        ]);
+
+        $this->assertDatabaseHas('diaries', [
+            'date' => '2022-06-05'
+        ]);
+    }
+
+    public function testGpVisitInDatabase()
+    {
+        $this->post('computeRegister', [
+            'username' => 'Ben10',
+            'password' => 'Daniel1',
+            'password1' => 'Daniel1'
+        ]);
+
+        $this->get('/gpTracker');
+        $this->get('/addGPVisit');
+        $this->post('/addGPVisit/entry', [
+            'date' => '2022-06-05',
+            'comment' => 'Roy Keane',
+
+        ]);
+
+        $this->assertDatabaseHas('g_p_trackers', [
+            'date' => '2022-06-05'
+        ]);
+    }
+
+    public function testDiaryUpdatesInDatabase()
+    {
+        $this->post('computeRegister', [
+            'username' => 'Ben10',
+            'password' => 'Daniel1',
+            'password1' => 'Daniel1'
+        ]);
+
+        $this->get('/addDiary');
+        $this->post('addDiary/entry', [
+            'date' => '2022-06-05',
+            'stress' => 1,
+
+        ]);
+
+        $this->get('/updateDiary/1');
+        $this->put('/completeUpdateDiary/1', [
+            'date' => '2022-06-04',
+            'chocolate' => 1,
+            'comment' => 'Eric Cantona'
+        ]);
+
+        $this->assertDatabaseHas('diaries', [
+            'date' => '2022-06-04'
+        ]);
+
+    }
+
+    public function testGpVisitUpdateInDatabase()
+    {
+        $this->post('computeRegister', [
+            'username' => 'Ben10',
+            'password' => 'Daniel1',
+            'password1' => 'Daniel1'
+        ]);
+
+        $this->get('/gpTracker');
+        $this->get('/addGPVisit');
+        $this->post('/addGPVisit/entry', [
+            'date' => '2022-06-05',
+            'comment' => 'Roy Keane',
+
+        ]);
+
+        $this->get('/updateGPVisit/1');
+        $this->put('/completeGPVisitUpdate/1', [
+            'date' => '2022-06-04',
+            'comment' => 'Eric Cantona'
+        ]);
+
+        $this->assertDatabaseHas('g_p_trackers', [
+            'date' => '2022-06-04'
+        ]);
+    }
+
+    public function testDeletedDiaryEntryNotInDatabase()
+    {
+        $this->post('computeRegister', [
+            'username' => 'Ben10',
+            'password' => 'Daniel1',
+            'password1' => 'Daniel1'
+        ]);
+
+        $this->get('/addDiary');
+        $this->post('addDiary/entry', [
+            'date' => '2022-06-05',
+            'stress' => 1,
+
+        ]);
+        
+        $this->get('/diary');
+        $this->get('/deleteDiaryEntry/1');
+
+        $this->assertDatabaseMissing('diaries', [
+            'date' => '2022-06-05'
+        ]);
+    }
+
+    public function testDeletedGpVisitEntryNotInDatabase()
+    {
+        $this->post('computeRegister', [
+            'username' => 'Ben10',
+            'password' => 'Daniel1',
+            'password1' => 'Daniel1'
+        ]);
+
+        $this->get('/gpTracker');
+        $this->get('/addGPVisit');
+        $this->post('/addGPVisit/entry', [
+            'date' => '2022-06-05',
+            'comment' => 'Roy Keane',
+
+        ]);
+
+        $this->get('/gpTracker');
+        $this->get('/deleteGPVisit/1');
+
+        $this->assertDatabaseMissing('g_p_trackers', [
+            'date' => '2022-06-05'
+        ]);
+    }
+
+    public function testDataIsBeingDisplayedForDiary()
+    {
+        $this->post('computeRegister', [
+            'username' => 'Ben10',
+            'password' => 'Daniel1',
+            'password1' => 'Daniel1'
+        ]);
+
+        $this->get('/addDiary');
+        $this->post('addDiary/entry', [
+            'date' => '2022-06-05',
+            'stress' => 1,
+
+        ]);
+        
+        $response = $this->get('/diary');
+
+        $response->assertSeeText('05-06-2022');
+
+    }
+
+    public function testDataIsBeingDisplayedForGpTracker()
+    {
+        $this->post('computeRegister', [
+            'username' => 'Ben10',
+            'password' => 'Daniel1',
+            'password1' => 'Daniel1'
+        ]);
+
+        $this->get('/gpTracker');
+        $this->get('/addGPVisit');
+        $this->post('/addGPVisit/entry', [
+            'date' => '2022-06-05',
+            'comment' => 'Roy Keane',
+
+        ]);
+
+        $response = $this->get('/gpTracker');
+
+        $response->assertSeeText('05-06-2022');
+    }
+
+    public function testUpdatedDataIsDisplayedForDiary()
+    {
+        $this->post('computeRegister', [
+            'username' => 'Ben10',
+            'password' => 'Daniel1',
+            'password1' => 'Daniel1'
+        ]);
+
+        $this->get('/addDiary');
+        $this->post('addDiary/entry', [
+            'date' => '2022-06-05',
+            'stress' => 1,
+
+        ]);
+
+        $this->get('/updateDiary/1');
+        $this->put('/completeUpdateDiary/1', [
+            'date' => '2022-06-04',
+            'chocolate' => 1,
+            'comment' => 'Eric Cantona'
+        ]);
+        
+        $response = $this->get('/diary');
+
+        $response->assertSeeText('04-06-2022');
+
+    }
+
+    public function testUpdatedDataIsDisplayedForGpTracker()
+    {
+        $this->post('computeRegister', [
+            'username' => 'Ben10',
+            'password' => 'Daniel1',
+            'password1' => 'Daniel1'
+        ]);
+
+        $this->get('/gpTracker');
+        $this->get('/addGPVisit');
+        $this->post('/addGPVisit/entry', [
+            'date' => '2022-06-05',
+            'comment' => 'Roy Keane',
+
+        ]);
+
+        $this->get('/updateGPVisit/1');
+        $this->put('/completeGPVisitUpdate/1', [
+            'date' => '2022-06-04',
+            'comment' => 'Eric Cantona'
+        ]);
+
+        $response = $this->get('/gpTracker');
+
+        $response->assertSeeText('04-06-2022');
+    }
+
+    public function testDeletedDiaryDataIsNotDisplayed()
+    {
+        $this->post('computeRegister', [
+            'username' => 'Ben10',
+            'password' => 'Daniel1',
+            'password1' => 'Daniel1'
+        ]);
+
+        $this->get('/addDiary');
+        $this->post('addDiary/entry', [
+            'date' => '2022-06-05',
+            'stress' => 1,
+
+        ]);
+        
+        $this->get('/diary');
+        $this->get('/deleteDiaryEntry/1');
+
+        $response = $this->get('/diary');
+        $response->assertDontSeeText('05-06-2022');
+    }
+
+    public function testDeletedGpVisitEntryIsNotDisplayed()
+    {
+        $this->post('computeRegister', [
+            'username' => 'Ben10',
+            'password' => 'Daniel1',
+            'password1' => 'Daniel1'
+        ]);
+
+        $this->get('/gpTracker');
+        $this->get('/addGPVisit');
+        $this->post('/addGPVisit/entry', [
+            'date' => '2022-06-05',
+            'comment' => 'Roy Keane',
+
+        ]);
+
+        $this->get('/gpTracker');
+        $this->get('/deleteGPVisit/1');
+
+        $response = $this->get('/gpTracker');
+        $response->assertDontSeeText('05-06-2022');
+    }
+
+    public function testAnalysisResultsAreDisplayed()
+    {
+        $this->post('computeRegister', [
+            'username' => 'Ben10',
+            'password' => 'Daniel1',
+            'password1' => 'Daniel1'
+        ]);
+
+        $this->get('/addDiary');
+        $this->post('addDiary/entry', [
+            'date' => '2022-06-05',
+            'stress' => 1,
+
+        ]);
+
+        $this->post('addDiary/entry', [
+            'date' => '2022-06-05',
+            'stress' => 0,
+
+        ]);
+
+        $response = $this->get('/analysis');
+
+        $response->assertSeeText('50%');
+    }
+
+    public function testHomepageDisplaysText()
+    {
+        $response = $this->get('/');
+
+        $response->assertSeeText('Welcome to this Online Migraine Diary!');
     }
 
     /*
